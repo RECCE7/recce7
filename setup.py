9b808e3d45597df6d4c149ae8096ff0da97b52ac
jnels124 <jnels1242012@gmail.com>
2016-05-13 23:47:56 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='recce7',
       version='1.0',
-      description='Report Server',
+      description='Honey Pot',
       author='Jesse Nelson',
       author_email='jnels124@msudenver.edu',
       data_files=[('/etc/recce7/configs/', ['install/configs/plugins.cfg', 'install/configs/global.cfg']),
